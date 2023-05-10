a16d958d6903edc30876e4e881340f85f36896ac
MalakMelhem <malak.melhem98@gmail.com>
2023-05-10 21:51:10 +0300

    added the router first commit

---
+++ src/Router.js
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import React from 'react'
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {/* <Route index element={<Home />} />   */}
+          {/* <Route path="shop" element={<Shop />} /> */}
+          {/* <Route path="singleProduct" element={<SingleProduct />} /> */}
+          {/* <Route path="myAccount" element={<MyAccount />} /> */}
+          {/* <Route path="cart" element={<Cart />} /> */}
+          {/* <Route path="*" element={<NoPage />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default Router;
