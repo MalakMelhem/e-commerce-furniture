55fed1f6641f0cb1301779d21e93885e7560389b
MalakMelhem <malak.melhem98@gmail.com>
2023-05-21 14:57:42 +0300

    Using Array.map() to present the items in PurchasingBanner

--- src/components/PurchasingBanner/PurchasingBanner.js
+++ src/components/PurchasingBanner/PurchasingBanner.js
@@ -2,12 +2,13 @@ import React from 'react';
 import style from './PurchasingBanner.module.css';
 import { Box } from '@mui/material';
 import Typography from '@mui/material/Typography';
+
+const PurchasingBannerData=[{title:"Free Delivery",desc:"For all oders over $50, consectetur adipim scing elit."},{title:"90 Days Return" ,desc:"If goods have problems, consectetur adipim scing elit."},{title:"Secure Payment", desc:"100% secure payment, consectetur adipim scing elit."}];
+
 const PurchasingBanner = () => {
   return (
     <Box className={style.purchasingBanner}>
-        <PurchasingContainer className='delivery' title="Free Delivery" desc="For all oders over $50, consectetur adipim scing elit."/>
-        <PurchasingContainer className='return' title="90 Days Return" desc="If goods have problems, consectetur adipim scing elit."/>
-        <PurchasingContainer className='payment' title="Secure Payment" desc="100% secure payment, consectetur adipim scing elit."/>
+      {PurchasingBannerData.map((data,index)=>(<PurchasingContainer key={index} title={data.title} desc={data.desc}/>)) }
     </Box>
   );
 }
