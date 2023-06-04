fbb5f861247a18f4f7eae7f37aa089a57e400fec
MalakMelhem <malak.melhem98@gmail.com>
2023-06-04 17:55:37 +0300

    Updates to the width and other CSS properties to fix the responsive to the small screens, in the single product page and not found page, and an update to the reset password page

--- src/pages/LostPassword.js
+++ src/pages/LostPassword.js
@@ -1,9 +1,15 @@
 import { Box } from '@mui/material';
-import React from 'react';
+import React,{useEffect} from 'react';
 import ResetPassword from '../components/ResetPassword/ResetPassword';
 
 
 const LostPassword = () => {
+
+  useEffect(() => {
+    // Scroll to the top of the page when the component mounts
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <Box>
         <ResetPassword />
